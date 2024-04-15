93c8fcf8f9effbe6b49cad68a682a4880d2884ee
yoshihiro_yoneyama <yoshihiro.yoneyama@salesnavi.co.jp>
2024-04-15 23:14:11 +0900

    IdFactoryConfigurationにTaskIdのBeanを追加

--- web-api/src/main/java/task/webaip/infrastructure/util/IdFactoryConfiguration.java
+++ web-api/src/main/java/task/webaip/infrastructure/util/IdFactoryConfiguration.java
@@ -1,7 +1,9 @@
 package task.webaip.infrastructure.util;
 
 import lombok.NonNull;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import task.webaip.domain.task.TaskId;
 import task.webaip.lib.IdFactory;
 
 import java.util.UUID;
@@ -11,5 +13,10 @@ import java.util.function.Function;
 public class IdFactoryConfiguration {
   private static <T> IdFactory<T> uuidFactory(final @NonNull Function<String, T> f) {
     return () -> f.apply(UUID.randomUUID().toString());
+  }
+
+  @Bean
+  IdFactory<TaskId> taskId() {
+    return uuidFactory(TaskId::reconstruct);
   }
 }
